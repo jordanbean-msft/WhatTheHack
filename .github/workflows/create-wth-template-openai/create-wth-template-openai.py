b9df356e4e70dc346883a93de5b19a5b4b0025c3
Jordan Bean <jordanbean@microsoft.com>
2023-06-26 14:24:52 -0500

    working

--- .github/workflows/create-wth-template-openai/create-wth-template-openai.py
+++ .github/workflows/create-wth-template-openai/create-wth-template-openai.py
@@ -10,29 +10,20 @@ import shutil
 template_directory_name = "000-HowToHack"
 openai_prompt_directory_name = "openai-prompts"
 
-root_path = ""
-verbosityArg = False
-path_to_openai_prompt_directory = ""
-description_of_hack = ""
-keywords = ""
-openai_hack_description = ""
-openai_endpoint_uri = ""
-openai_api_key = ""
-
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         usage="%(prog)s [OPTION] [FILE]...",
         description="Create a new hackathon project from a template using OpenAI's API.")
     
     parser.add_argument("-c", "--number_of_challenges", help="Number of challenges to generate", type=int)
-    parser.add_argument("-r", "--remove_existing_directory", help="Remove existing directory", type=bool, default=True)
+    parser.add_argument("-r", "--remove_existing_directory", help="Remove existing directory", action='store_true')
     parser.add_argument("-n", "--name_of_hack", help="Name of hackathon project", type=str)
     parser.add_argument("-p", "--path_to_hack", help="Path to hackathon project", type=str)
     parser.add_argument("-d", "--description_of_hack", help="Description of hackathon project", type=str)
     parser.add_argument("-k", "--keywords", help="Keywords for hackathon project", type=str)
     parser.add_argument("-e", "--openai_endpoint_uri", help="OpenAI endpoint URI (should include API version query parameter)", type=str)
-    parser.add_argument("-t", "--openai_api_key", help="OpenAI API key", type=str)
-    parser.add_argument("-v", "--verbose", help="Verbose logging", type=bool, default=False)
+    parser.add_argument("-a", "--openai_api_key", help="OpenAI API key", type=str)
+    parser.add_argument("-v", "--verbose", help="Verbose logging", action='store_true')
     return parser
 
 def get_openai_prompt_content(path_to_prompt_file) -> str:
@@ -69,7 +60,13 @@ def call_openai(message_array) -> str:
 
     headers = { "Content-Type": "application/json", "api-key": f"{openai_api_key}" }
 
+    logging.info(f"Calling OpenAI API with the following data: {data}")
+
     openai_response = requests.post(openai_endpoint_uri, headers=headers, data=data)
+
+    if(openai_response.status_code != 200):
+        logging.error(f"OpenAI API call failed with status code: {openai_response.status_code}")
+        logging.error(f"OpenAI API call failed with response: {openai_response.text}")
 
     json_data = json.loads(openai_response.text)
 
@@ -77,22 +74,19 @@ def call_openai(message_array) -> str:
 
     return content
 
-
 def create_directory_structure(delete_existing_directory):
     if delete_existing_directory:
         shutil.rmtree(root_path)
     
-    if verbosityArg:
-        print(f"Creating {root_path} directory...")
+    logging.info(f"Creating {root_path} directory...")
         
     # create the xxx-YetAnotherWth directory
     os.mkdir(root_path)
 
-    if verbosityArg:
-        print(f"Creating {root_path}/Coach/Solutions directories...")
+    logging.info(f"Creating {root_path}/Coach/Solutions directories...")
         
     # create the Coach & Coach/Solutions directories
-    os.mkdir(f"{root_path}/Coach/Solutions")
+    os.makedirs(f"{root_path}/Coach/Solutions")
 
     #add a file to allow git to store an "empty" directory
     open(f"{root_path}/Coach/Solutions/.gitkeep", 'w').close()
@@ -100,11 +94,10 @@ def create_directory_structure(delete_existing_directory):
     # copying the Coach Lectures template file in the /Coach directory
     shutil.copy(f"{template_directory_name}/WTH-Lectures-Template.pptx", f"{root_path}/Coach/Lectures.pptx")
 
-    if verbosityArg:
-        print(f"Creating {root_path}/Student/Resources directories...")
+    logging.info(f"Creating {root_path}/Student/Resources directories...")
         
     # create the Student & Student/Resources directories
-    os.mkdir(f"{root_path}/Student/Resources")
+    os.makedirs(f"{root_path}/Student/Resources")
     
     #add a file to allow git to store an "empty" directory
     open(f"{root_path}/Student/Resources/.gitkeep", 'w').close()
@@ -132,25 +125,22 @@ def create_challenge_markdown_file(full_path, prefix, suffix_number) -> str:
 
     return openai_response
 
-def create_solution_markdown_file(full_path,prefix,suffix_number,challenge_response) -> str:
+def create_solution_markdown_file(full_path, prefix, suffix_number, challenge_response) -> str:
     openai_response = write_markdown_file(f"{path_to_openai_prompt_directory}/WTH-Solution-Prompt.txt", f"Generate a coach's guide solution page. It should be the step-by-step solution guide based upon the following challenge description: {challenge_response}", f"{full_path}/{prefix}-{suffix_number.md}")
 
     return openai_response
 
 def create_challenge_and_solution(challenge_number):
-    if verbosityArg:
-        print(f"Creating {root_path}/Challenge-{challenge_number}.md...")
+    logging.info(f"Creating {root_path}/Challenge-{challenge_number}.md...")
     
     challenge_response = create_challenge_markdown_file(f"{root_path}/Student", "Challenge", challenge_number)
 
-    if verbosityArg:
-        print(f"Creating {root_path}/Solution-{challenge_number}.md...")
+    logging.info(f"Creating {root_path}/Solution-{challenge_number}.md...")
 
     create_solution_markdown_file(f"{root_path}/Coach", "Solution", challenge_number, challenge_response)
 
-def create_coach_guide_markdown_file(full_path,number_of_solutions) -> str:
-    if verbosityArg:
-        print(f"Creating {full_path}/README.md...")
+def create_coach_guide_markdown_file(full_path, number_of_solutions) -> str:
+    logging.info(f"Creating {full_path}/README.md...")
 
     openai_response = write_markdown_file(f"{path_to_openai_prompt_directory}/WTH-Coach-Overview-Prompt.txt", "Generate a coach's guide overview page of the hack based upon the following description: {openai_hack_description}", f"{full_path}/README.md")
 
@@ -166,26 +156,36 @@ def main(argv):
     parser = init_argparse()
     args = parser.parse_args(argv)
 
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
     wth_directory_name = f"xxx-{args.name_of_hack}"
 
+    global verbosity
     verbosity = args.verbose
 
-    root_path = f"{args.path}/{wth_directory_name}"
+    global root_path
+    root_path = f"{args.path_to_hack}/{wth_directory_name}"
 
-    path_to_template_directory = f"{args.path}/{args.template_directory_name}"
+    path_to_template_directory = f"{args.path_to_hack}/{template_directory_name}"
 
+    global path_to_openai_prompt_directory
     path_to_openai_prompt_directory = f"{path_to_template_directory}/{openai_prompt_directory_name}"
 
+    global description_of_hack
     description_of_hack = f"{args.description_of_hack}"
 
+    global keywords
     keywords = f"{args.keywords}"
 
+    global openai_endpoint_uri
     openai_endpoint_uri = f"{args.openai_endpoint_uri}"
 
+    global openai_api_key
     openai_api_key = f"{args.openai_api_key}"
 
     create_directory_structure(args.remove_existing_directory)
 
+    global openai_hack_description
     openai_hack_description = create_hack_description(args.number_of_challenges)
 
     create_challenges_and_solutions(args.number_of_challenges)
